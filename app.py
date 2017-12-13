09c4892b5f1f03e49eda684dd22dec20b322b5eb
Richard Farman <farmanrl@whitman.edu>
2017-12-13 10:24:59 -0800

    Change endpoint names to be more descriptive.

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 import rosswriter as rwriter
 import plotwriter as pwriter
 import lyricwriter as lwriter
- 
-from flask import Flask, request
- 
+
+from flask import Flask
+
 # this is how we initialize a flask application
 app = Flask(__name__)
- 
-@app.route("/rwriter", methods=["GET"])
+
+
+@app.route("/ross", methods=["GET"])
 def gen_ross():
     sentence = rwriter.main()
     return(sentence)
 
-@app.route("/pwriter/<string:ppoint>", methods=["GET"])
+
+@app.route("/lyric/<string:ppoint>", methods=["GET"])
 def gen_plot(ppoint):
     sentence = pwriter.main(ppoint)
     return(sentence)
 
-@app.route("/lwriter/<string:iartist>", methods=["GET"])
+
+@app.route("/plot/<string:iartist>", methods=["GET"])
 def gen_lyrics(iartist):
     sentence = lwriter.main(iartist)
     return(sentence)
- 
+
+
 if __name__ == "__main__":
     """
     this is run when the script is started.
     """
- 
+
     # this is how we run the flask server, once the script is run
     app.run(host='0.0.0.0', threaded=True)
