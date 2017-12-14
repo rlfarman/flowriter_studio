c2c7bc0a92c00701810d306167bf2f690c003b1d
Zach Turner <eventhisone@gmail.com>
2017-12-13 17:14:52 -0800

    Added doc strings to the functions in app.py

--- app.py
+++ app.py
@@ -9,16 +9,20 @@ app = Flask(__name__)
  
 @app.route("/ross", methods=["GET"])
 def gen_ross():
+    """Takes no input. Returns a Simulated Bob Ross statement using a markov chain."""
     sentence = rwriter.main()
     return(sentence)
 
 @app.route("/plot/<string:plot_point>", methods=["GET"])
 def gen_plot(plot_point):
+    """Takes a plot point as an arguement: inciting-event, first-plot-point, first-pinch-point, midpoint, 
+        second-pinch-point, third-plot-point, climax, climactic-moment, resolution, or notes. Returns a generated plot point."""
     sentence = pwriter.main(ppoint)
     return(sentence)
 
 @app.route("/lyric/<string:artist>", methods=["GET"])
 def gen_lyrics(artist):
+    """Takes an artist name as an input. Returns three lines of generated lyrics based on the artist."""
     sentence = lwriter.main(iartist)
     return(sentence)
  
