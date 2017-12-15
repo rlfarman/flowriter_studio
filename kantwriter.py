b41002b4f5c478e00c3b0494a13fad7a13f08a76
Jesse Bulson-Lewis <bulsonjg@whitman.edu>
2017-12-15 04:09:50 -0800

    Add downloader/parser function
    A monolithic funtion like this seemed like the cleanest solution to
    parsing the text files from Project Gutenberg, i.e. removing their pre-
    and post-ambles. Without doing it all at once, either more data would
    have to be passed in between functions or multiple read/writes would be
    performed, decreasing performance.

--- kantwriter.py
+++ kantwriter.py
@@ -1,4 +1,10 @@
+import logging
+import urllib.request
 import hashlib
+
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 
 available_works = {
@@ -34,3 +40,34 @@ def get_model_name(title):
     file_name = get_file_name(title)
     model_name = file_name + '.model'
     return model_name
+
+
+def download_work(title, file_name):
+    """Parameters: title that is a key in "available_works"
+    Returns: none, downloads work to "file_name"
+    NOTE: text parsing is included here to deal with info from the sources"""
+    log.debug('Downloading text of "%s"', title)
+    response = urllib.request.urlopen(available_works[title])
+    lines = response.readlines()
+    # Text begins with Gutenberg info
+    in_body = False
+    info_end = '*** START OF THIS PROJECT GUTENBERG EBOOK '
+    info_begin = 'End of Project Gutenberg\'s '
+    write_buffer = ""
+    for data in lines:
+        line = data.decode('utf-8')
+        # If inside the actual text
+        if in_body:
+            # Make sure the current line is actual text
+            if info_begin in line:
+                log.debug('Reached end of body in "%s"', title)
+                break
+            # If so, save the line
+            else:
+                write_buffer += line
+        # Otherwise check to see if line is end of info section
+        else:
+            in_body = info_end in line
+    file_ = open(file_name, 'w')
+    file_.write(write_buffer)
+    file_.close()
