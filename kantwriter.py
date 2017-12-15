d65f3c04284bd6e31b21fc00dc5ca124b247803a
Jesse Bulson-Lewis <bulsonjg@whitman.edu>
2017-12-15 05:08:58 -0800

    Remove books causing errors on CentOS
    Books do not download/parse correctly, resulting in empty text files
    that of course cannot be modeled

--- kantwriter.py
+++ kantwriter.py
@@ -13,9 +13,7 @@ log = logging.getLogger(__name__)
 
 available_works = {
     'critique of pure reason': 'http://www.gutenberg.org/cache/epub/4280/pg4280.txt',
-    'critique of practical reason': 'http://www.gutenberg.org/cache/epub/5683/pg5683.txt',
-    'critique of judgment': 'http://www.gutenberg.org/files/48433/48433-0.txt',
-    'prolegomena': 'http://www.gutenberg.org/files/52821/52821-0.txt'
+    'critique of practical reason': 'http://www.gutenberg.org/cache/epub/5683/pg5683.txt'
     }
 
 
