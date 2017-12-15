7336cda5c2412c80d127c4394aa1836b1a5d4deb
Jesse Bulson-Lewis <bulsonjg@whitman.edu>
2017-12-15 05:59:21 -0800

    Add Kant module to app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import rosswriter as rwriter
 import plotwriter as pwriter
 import lyricwriter as lwriter
+import kantwriter as kwriter
 
 from flask import Flask, request
 
@@ -27,6 +28,30 @@ def gen_lyrics(artist):
     """Takes an artist name as an input. Returns three lines of generated lyrics based on the artist."""
     sentence = lwriter.main(artist)
     return(sentence)
+
+@app.route("/kant/available", methods=["GET"])
+def get_titles():
+    """Return list of available titles"""
+    list = kwriter.get_available_works()
+    return(list)
+
+
+@app.route("/kant/short_sentence/<string:title>", methods=["GET"])
+def gen_kant_short_sentence(title):
+    if title in kwriter.get_available_works():
+        sentence = kwriter.get_short_sentence(title)
+        return(sentence)
+    else:
+        return('Please submit only titles from /kant/available'), 400
+
+@app.route("/kant/sentence/<string:title>", methods=["GET"])
+def gen_kant_sentence(title):
+    if title in kwriter.get_available_works():
+        sentence = kwriter.get_sentence(title)
+        return(sentence)
+    else:
+        return('Please submit only titles from /kant/available'), 400
+
 
 @app.errorhandler(404)
 def not_found(error):
